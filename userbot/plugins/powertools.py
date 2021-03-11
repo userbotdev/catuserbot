998345a68a6ca580df45e46122b6a90277575a98
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-11 16:39:23 +0300

    ICSS

--- userbot/plugins/powertools.py
+++ userbot/plugins/powertools.py
@@ -5,51 +5,39 @@ from time import sleep
 from . import BOTLOG, BOTLOG_CHATID, HEROKU_APP, bot
 
 
-@bot.on(admin_cmd(pattern="restart$"))
-@bot.on(sudo_cmd(pattern="restart$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="اعادة تشغيل$"))
+@bot.on(sudo_cmd(pattern="اعادة تشغيل$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted")
+        await event.client.send_message(
+            BOTLOG_CHATID, "#اعادة_التشغيل \n" "⪼ بوت اكسس في وضع اعادة التشغيل انتظر"
+        )
     await edit_or_reply(
         event,
-        "Restarted. `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting",
+        "**اعادة التشغيل** ↫ استخدم `.ايدي` او `.بنك` للتحقق مما إذا كنت متصلاً بالإنترنت ، يستغرق الأمر في الواقع 1-2 دقيقة لإعادة التشغيل",
     )
     await bot.disconnect()
     execl(sys.executable, sys.executable, *sys.argv)
 
 
-@bot.on(admin_cmd(pattern="shutdown$"))
-@bot.on(sudo_cmd(pattern="shutdown$", allow_sudo=True))
+@bot.on(admin_cmd(pattern="ايقاف$"))
+@bot.on(sudo_cmd(pattern="ايقاف$", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n" "Bot shut down")
-    await edit_or_reply(event, "`Turning off bot now ...Manually turn me on later`")
-    if HEROKU_APP is not None:
-        HEROKU_APP.process_formation()["worker"].scale(0)
-    else:
-        sys.exit(0)
-
-
-@bot.on(admin_cmd(pattern="sleep( [0-9]+)?$"))
-@bot.on(sudo_cmd(pattern="sleep( [0-9]+)?$", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    if " " not in event.pattern_match.group(1):
-        return await edit_or_reply(event, "Syntax: `.sleep time`")
-    counter = int(event.pattern_match.group(1))
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID,
-            "You put the bot to sleep for " + str(counter) + " seconds",
+            BOTLOG_CHATID, "#اطفاء \n" "بوت اكسس في وضع الاطفاء"
         )
-    event = await edit_or_reply(event, f"`ok, let me sleep for {counter} seconds`")
-    sleep(counter)
-    await event.edit("`OK, I'm awake now.`")
+    await edit_or_reply(
+        event, "**جارٍ إيقاف تشغيل بوت اكسس الآن ... شغِّلني يدويًا لاحقًا**"
+    )
+    if HEROKU_APP is not None:
+        HEROKU_APP.process_formation()["userbot"].scale(0)
+    else:
+        sys.exit(0)
 
 
 CMD_HELP.update(
