42b2444fee7061ed606bcd582e33255300bd11f8
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-11 14:17:49 +0300

    ICSS

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,14 +5,14 @@ from telethon import version
 
 from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, reply_id
 
-DEFAULTUSER = ALIVE_NAME or "cat"
-CAT_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
+DEFAULTUSER = ALIVE_NAME or "ICSS"
+CAT_IMG = Config.ALIVE_PIC or "https://telegra.ph/file/499596b18292c0e43ac56.jpg"
+CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪"
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  - ❝ ⌊ "
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="السورس$"))
+@bot.on(sudo_cmd(pattern="السورس$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
@@ -20,13 +20,16 @@ async def amireallyalive(alive):
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     if CAT_IMG:
-        cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-        cat_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
-        cat_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-        cat_caption += f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
-        cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-        cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
-        cat_caption += f"**{EMOJI} Master:** {mention}\n"
+        cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n"
+        cat_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+        cat_caption += f"**{EMOJI} قاعدة البيانات ↫** `{check_sgnirts}`\n"
+        cat_caption += f"**{EMOJI} اصدار التليثون  ↫** `{version.__version__}\n`"
+        cat_caption += f"**{EMOJI} اصدار اڪسس ↫** `{catversion}`\n"
+        cat_caption += f"**{EMOJI} اصدار البايثون ↫** `{python_version()}\n`"
+        #        cat_caption += f"**{EMOJI} مدة التشغيل ↫** `{uptime}\n`"
+        cat_caption += f"**{EMOJI} المستخدم ↫** {mention}\n"
+        cat_caption += f"**{EMOJI} مطور السورس ↫** [اضغط هنا](t.me/rruuurr) 𓆰.\n"
+        cat_caption += f"𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𝐢𝐜𝐬𝐬ⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
         await alive.client.send_file(
             alive.chat_id, CAT_IMG, caption=cat_caption, reply_to=reply_to_id
         )
@@ -35,47 +38,36 @@ async def amireallyalive(alive):
         await edit_or_reply(
             alive,
             f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} Database :** `{check_sgnirts}`\n"
-            f"**{EMOJI} Telethon Version :** `{version.__version__}\n`"
-            f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
-            f"**{EMOJI} Python Version :** `{python_version()}\n`"
-            f"**{EMOJI} Uptime :** `{uptime}\n`"
-            f"**{EMOJI} Master:** {mention}\n",
+            f"**{EMOJI} قاعدة البيانات ↫**  `{check_sgnirts}`\n"
+            f"**{EMOJI} اصدار التليثون  ↫** `{version.__version__}\n`"
+            f"**{EMOJI} اصدار اڪسس ↫** `{catversion}`\n"
+            f"**{EMOJI} اصدار البايثون  ↫** `{python_version()}\n`"
+            f"**{EMOJI} مدة التشغيل ↫** `{uptime}\n`"
+            f"**{EMOJI} المستخدم ↫** {mention}\n",
         )
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="ialive$"))
-@bot.on(sudo_cmd(pattern="ialive$", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="البوت$"))
+@bot.on(sudo_cmd(pattern="البوت$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
     tgbotusername = Config.TG_BOT_USERNAME
     reply_to_id = await reply_id(alive)
-    cat_caption = f"**Catuserbot is Up and Running**\n"
-    cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**  -Catuserbot Version :** `{catversion}`\n"
-    cat_caption += f"**  -Python Version :** `{python_version()}\n`"
-    cat_caption += f"**  -Master:** {mention}\n"
+    cat_caption = f"𓆩 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 𝑻𝑶 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺 𓆪\n"
+    cat_caption += f"**  - اصدار التليثون ↫** `{version.__version__}\n`"
+    cat_caption += f"**  - اصدار اكسس ↫** `{catversion}`\n"
+    cat_caption += f"**  - اصدار البايثون ↫** `{python_version()}\n`"
+    cat_caption += f"**  - المستخدم ↫** {mention}\n"
     results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
     await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
     await alive.delete()
 
 
-# UniBorg Telegram UseRBot
-# Copyright (C) 2020 @UniBorg
-# This code is licensed under
-# the "you can't use this for anything - public or private,
-# unless you know the two prime factors to the number below" license
-# 543935563961418342898620676239017231876605452284544942043082635399903451854594062955
-# വിവരണം അടിച്ചുമാറ്റിക്കൊണ്ട് പോകുന്നവർ
-# ക്രെഡിറ്റ് വെച്ചാൽ സന്തോഷമേ ഉള്ളു..!
-# uniborg
-
-
 def check_data_base_heal_th():
     # https://stackoverflow.com/a/41961968
     is_database_working = False
-    output = "No Database is set"
+    output = "لم يتم تعيين قاعدة بيانات"
     if not Config.DB_URI:
         return is_database_working, output
     from userbot.plugins.sql_helper import SESSION
@@ -87,7 +79,7 @@ def check_data_base_heal_th():
         output = f"❌ {str(e)}"
         is_database_working = False
     else:
-        output = "Functioning Normally"
+        output = "تعمل بنجاح"
         is_database_working = True
     return is_database_working, output
 
