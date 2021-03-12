3acd3e512dbb727296a27361814279f9299b25a4
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-12 07:52:32 +0300

    ICSS

--- userbot/plugins/kk.py
+++ userbot/plugins/kk.py
@@ -25,6 +25,51 @@ import asyncio
 
 @bot.on(admin_cmd(pattern="تحديث", outgoing=True))
 async def icss(ics):
-    await ics.edit("**⪼ هذا الامر غير موجود اذا اردت معرفت السبب راسل المطور 𓆰")
+    await ics.edit("**⪼ هذا الامر غير موجود اذا اردت معرفت السبب راسل المطور 𓆰**")
     await asyncio.sleep(1)
-    await ics.edit("**⪼ هذا الامر غير موجود اذا اردت معرفت السبب راسل المطور 𓆰")
+    await ics.edit("**⪼ هذا الامر غير موجود اذا اردت معرفت السبب راسل المطور 𓆰**")
+
+@bot.on(admin_cmd(pattern="تيست", outgoing=True))
+async def icss(ics)
+    await ics.edit("**⪼ هذا الامر غير موجود اذا اردت معرفت السبب راسل المطور 𓆰**")
+    await asyncio.sleep(1)
+    await ics.edit("**⪼ هذا الامر غير موجود اذا اردت معرفت السبب راسل المطور 𓆰**")
+
+
+import os
+import random
+
+from telethon.tl.users import GetFullUserRequst
+from telethon.tl.types import MassageEntityMentoinName
+
+hbk = [
+  "100%"
+  "90%"
+  "80%"
+  "70%"
+  "60%"
+  "50%"
+  "40%"
+  "30%"
+  "20%"
+  "10%"
+  "0%"
+]
+
+@bot.on(admin_cmd(pattren="تيست", outgoing=True))
+async def icss(mentoin):
+uu = random.choice(hbk)
+    user, custom = await get_user_from_events(mention)
+    if not user:
+        return
+    if custom:
+       await edit_or_reply(
+           mention, f" تسيت [{custom}](tg://user?id={user?id}) {uu}"
+       )
+   else:
+       tag = (
+           user.first_name.replace("\u2060", "") if user.first_name else user.username
+       )
+       await edit_or_reply(
+           mention, f"تيست [{tag}](tg://user?id={user?id}) {uu}"
+       )
