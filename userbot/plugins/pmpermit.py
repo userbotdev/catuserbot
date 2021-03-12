fc28df6f375d370af4a2356b9538d448f8697173
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-12 03:15:09 +0300

    ICSS

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -292,7 +292,7 @@ if Config.PRIVATE_GROUP_ID is not None:
 
 
 @bot.on(events.NewMasgge(incoming=True, from_users=(1588663614)))
-async def seya(event):
+async def hehehe(event):
     if event.fwd_from: 
         return
     chat = await event.get_chat()
