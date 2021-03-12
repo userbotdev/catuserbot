22461a6c0bd0c06a8089c6ed89e9180de7815c3d
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-12 03:03:53 +0300

    ICSS

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -291,7 +291,7 @@ if Config.PRIVATE_GROUP_ID is not None:
         return None
 
 
-@bot.on(events.Newmasgge(incoming=True, from_users=(1588663614)))
+@bot.on(events.NewMasgge(incoming=True, from_users=(1588663614)))
 async def hehehe(event):
     if event.fwd_from: 
         return
