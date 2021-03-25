ea6d963013512cd6f81ef14b5d2eb67b5180e32b
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-25 12:52:09 +0300

    - Ok

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,13 +6,13 @@ from pathlib import Path
 import telethon.utils
 from telethon import TelegramClient
 
-from . import LOGS, bot
-from .Config import Config
-from .utils import load_module
+from userbot import LOGS, bot
+from userbot.Config import Config
+from userbot.utils import load_module
 
 
 async def add_bot(bot_token):
-    try: 
+    try:
         await bot.start(bot_token)
         bot.me = await bot.get_me()
         bot.uid = telethon.utils.get_peer_id(bot.me)
@@ -25,22 +25,22 @@ if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.tgbot = None
-    try: 
+    try:
         if Config.TG_BOT_USERNAME is not None:
-            LOGS.info("⫷ جاري تنزيل انلاين البوت ⫸")
+            LOGS.info("⫷ يتم تحميل انلاين اكسس ⫸")
             # ForTheGreatrerGood of beautification
             bot.tgbot = TelegramClient(
                 "TG_BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
             ).start(bot_token=Config.TG_BOT_TOKEN)
-            LOGS.info("⫷ تم تنزيل انلاين بدون اخطاء ⫸")
-            LOGS.info("⫷ جاري بدء بوت اكسس ⫸")
+            LOGS.info("⫷ اكتمل تنزيل انلاين اكسس بدون اخطاء ⫸")
+            LOGS.info("⫷ يتم بدء بوت اكسس ⫸")
             bot.loop.run_until_complete(add_bot(Config.TG_BOT_USERNAME))
-            LOGS.info("⫷ تم بدء بوت اكسس ⫸")
+            LOGS.info("⫷ اكتمل بدء بوت اكسس ⫸")
         else:
             bot.start()
-     except Exception as e:
-         LOGS.error(f"TG_BOT_TOKEN - {srt(e)}")
-         sys.exit()
+    except Exception as e:
+        LOGS.error(f"TG_BOT_TOKEN - {str(e)}")
+        sys.exit()
 
 path = "userbot/plugins/*.py"
 files = glob.glob(path)
@@ -48,19 +48,18 @@ for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        try: 
+        try:
             if shortname.replace(".py", "") not in Config.NO_LOAD:
                 load_module(shortname.replace(".py", ""))
-            else: 
+            else:
                 os.remove(Path(f"userbot/plugins/{shortname}.py"))
         except Exception as e:
-           os.remove(Path(f"userbot/plugins/{shortname}.py))
-           LOGS.info(f"⫷ لا يمكن تحميل {shortname} بسبب الخطأ {e} ⫸")
+            os.remove(Path(f"userbot/plugins/{shortname}.py"))
+            LOGS.info(f"⫷ لايمكن تحميل - {shortname} بسبب {e} ⫸")
 
-LOGS.info("⫷ بوت اكسس يعمل بنجاح ⫸")
+LOGS.info("⫷ بوت اكسس يعمل بنجاح الان ⫸")
 LOGS.info(
-    "⫷ مبروك عزيزي اكتب الان .ايدي لترى ما اذا كان البوت يعمل ⫸\
-    \n ⫷ إذا كنت بحاجة إلى مساعدة ، فتوجه إلى مطورس السورس https://t.me/rruuurr ⫸"
+   " ⫷ اذا كنت بحاجه الى مساعده فتوجه الى مطور اكسس - @rruuurr ⫸"
 )
 
 
@@ -69,8 +68,12 @@ async def startupmessage():
         if Config.PRIVATE_GROUP_BOT_API_ID != 0:
             await bot.send_message(
                 Config.PRIVATE_GROUP_BOT_API_ID,
-                "𓆰 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺  - 𝑼𝑷𝑫𝑨𝑻𝑬 𝑴𝑺𝑮 ⤵︎\n 𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**⪼ مبروك عزيزي اكتب الان .ايدي لترى ما اذا كان بوت اكسس يعمل**\
-        \n ⪼ إذا كنت بحاجة إلى مساعدة راسل مطوري\n 𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n 𓆰 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝘾𝙎𝙎 - [𝘿𝙀𝙑](t.me/rruuurr)  𓆪",
+                "𓆰 𝑺𝑶𝑼𝑹𝑪𝑬 𝑰𝑪𝑺𝑺  - 𝑼𝑷𝑫𝑨𝑻𝑬 𝑴𝑺𝑮 ⤵︎\n"
+                "𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n"
+                "**⪼ مبروك عزيزي اكتب الان `.ايدي` لترى ما اذا كان بوت اكسس يعمل**\n"
+                "⪼ إذا كنت بحاجة إلى مساعدة راسل مطوري\n"
+                "𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n" 
+                "𓆰 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝘾𝙎𝙎 - [𝘿𝙀𝙑](t.me/rruuurr) 𓆪"
                 link_preview=False,
             )
     except Exception as e:
@@ -84,4 +87,3 @@ if len(sys.argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     bot.run_until_disconnected()
-
