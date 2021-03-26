647ace8020ae5eddf91fca762021b8894eb6ecd5
- For her #Busy <79422815+ANL0KE@users.noreply.github.com>
2021-03-26 22:12:43 +0300

    - Ok

---
+++ userbot/plugins/kim.py
@@ -0,0 +1,24 @@
+#    IcssBot - UserBot
+#    (c) @rruuurr
+
+
+from userbot import CMD_HELP
+from userbot.Config import Config
+
+
+@bot.on(
+    admin_cmd(
+       pattern="اكس او$"
+    )
+)
+
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
